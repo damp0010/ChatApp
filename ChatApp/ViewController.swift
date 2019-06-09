651eeddb9e4d1db03914dba37ca091c0503dca82
damp0010 <end.of.the.road.10@gmail.com>
2019-06-09 09:18:08 +0900

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by 田中　徳充 on 2019/06/09.
+//  Copyright © 2019 田中　徳充. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
